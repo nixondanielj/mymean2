3282d4b9569e97518e8482db8da5a5d1c92e755b
Dan Nixon <nixon.daniel.j@gmail.com>
2015-03-29 15:46:14 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,40 @@
+// setup
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash = require('connect-flash');
+
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+
+// config
+var configDB = require('./config/database.js');
+mongoose.connect(configDB.url);
+
+//require('./config/passport')(passport);
+
+// express setup
+// logging
+app.use(morgan('dev'));
+// parse cookies
+app.use(cookieParser());
+// parse body - difference from bodyParser.<specific>Parser?
+app.use(bodyParser());
+// passport
+app.use(session({ secret: 'fakeSecret' }));
+app.use(passport.initialize);
+app.use(passport.session());
+// might kill this... uses session messages
+app.use(flash());
+
+// routing
+require('./app/routes.js')(app, passport);
+
+//launch
+app.listen(port, process.env.IP || '0.0.0.0', function(){
+    console.log('Running...');
+});
